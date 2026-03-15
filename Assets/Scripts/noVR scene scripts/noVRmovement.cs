09e30fe8aa5de042f783f6e6aa3990e890e74b65
AJ-West <ashtonjwest2003@gmail.com>
2026-03-15 13:03:48 +0000

    added no VR scene for development

--- Assets/Scripts/noVR scene scripts/noVRmovement.cs
+++ Assets/Scripts/noVR scene scripts/noVRmovement.cs
@@ -1,9 +1,18 @@
 using UnityEngine;
+using static UnityEngine.XR.OpenXR.Features.Interactions.HandInteractionProfile;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+using UnityEngine.XR.Interaction.Toolkit.Interactors;
 
 public class noVRmovement : MonoBehaviour
 {
     public CharacterController characterController;
     public float playerSpeed = 5.0f;
+
+    public Transform cameraTransform;
+
+    [Header("Deck")]
+    public DeckManager deckManager;
+    public float Force = 100.0f;
 
     private Vector3 playerVelocity;
     private float gravity = -9.81f;
@@ -35,5 +44,34 @@ public class noVRmovement : MonoBehaviour
         }
 
         characterController.Move(playerVelocity * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("1");
+
+            // Ask DeckManager for the next card prefab
+            GameObject cardPrefab = deckManager.DrawCard();
+
+            if (cardPrefab == null)
+                return;
+
+            Debug.Log("2");
+
+            Vector3 position = transform.position + new Vector3(0, 1, 0);
+
+            // Spawn the card into the world
+            GameObject spawnedCard = Instantiate(cardPrefab, position, cameraTransform.rotation);
+
+            Rigidbody rb = spawnedCard.GetComponent<Rigidbody>();
+
+            if (rb == null)
+                return;
+
+            Debug.Log("3");
+
+            rb.AddForce(spawnedCard.transform.forward * Force, ForceMode.Force);
+
+            Debug.Log("4");
+        }
     }
 }
